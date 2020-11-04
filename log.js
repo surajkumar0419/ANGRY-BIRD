dd8ce1b42c3d778c4b2cf1102b2550b4ce351334
surajkumar0419 <73117869+surajkumar0419@users.noreply.github.com>
2020-11-04 19:05:07 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,29 @@
+class Log{
+    constructor(x,y,height,aNgle){
+        var options={
+            'restitution':0.5,
+            'density':1,
+            'friction':0.3
+
+        }
+        this.body=Bodies.rectangle(x,y,20,height,options);
+        this.width=20;
+        this.height=height;
+        Matter.Body.setAngle(this.body,aNgle);
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(4);
+        stroke("green");
+        fill("green");
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+    
+}
